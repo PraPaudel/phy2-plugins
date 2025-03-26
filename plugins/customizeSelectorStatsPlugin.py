c2935d0036fc980740644dde3207b96f355cf943
PraPaudel <prpaudel27@gmail.com>
2025-03-26 12:47:13 -0400

    Update customizeSelectorStatsPlugin.py

--- plugins/customizeSelectorStatsPlugin.py
+++ plugins/customizeSelectorStatsPlugin.py
@@ -8,7 +8,7 @@ class customizeSelectorStatsPlugin(IPlugin):
     def attach_to_controller(self, controller):
         @connect
         def on_controller_ready(sender):
-            controller.supervisor.columns = ['id', 'depth', 'fr', 'Amplitude', 'n_spikes', 'quality']
+            controller.supervisor.columns = ['id', 'fr', 'amp', 'sh', 'ch','n_spikes', 'quality']
 
             # controller.supervisor.columns = ['id', 'depth', 'fr', 'Amplitude', 'n_spikes', 'quality',
             # 'fractionRPV', 'percentSpikesMissing'] # can be used if used bombcell during preprocessing
