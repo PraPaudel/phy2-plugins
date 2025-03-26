fc4d4863c26840a3e17e949a7552a8b37c3d928a
PraPaudel <prpaudel27@gmail.com>
2025-03-26 12:26:01 -0400

    Create customizeSelectorStatsPlugin.py

---
+++ plugins/customizeSelectorStatsPlugin.py
@@ -0,0 +1,14 @@
+# import from plugins/custom_columns.py
+"""Show how to customize the columns in the cluster and similarity views."""
+
+from phy import IPlugin, connect
+
+
+class customizeSelectorStatsPlugin(IPlugin):
+    def attach_to_controller(self, controller):
+        @connect
+        def on_controller_ready(sender):
+            controller.supervisor.columns = ['id', 'depth', 'fr', 'Amplitude', 'n_spikes', 'quality']
+
+            # controller.supervisor.columns = ['id', 'depth', 'fr', 'Amplitude', 'n_spikes', 'quality',
+            # 'fractionRPV', 'percentSpikesMissing'] # can be used if used bombcell during preprocessing
