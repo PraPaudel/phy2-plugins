b2624512ceefaf8558f2451dc73be40eb45fd400
PraPaudel <prpaudel27@gmail.com>
2025-03-28 00:39:53 -0400

    Update phy_config.py

--- phy_config.py
+++ phy_config.py
@@ -21,7 +21,6 @@ c.TemplateGUI.plugins = [
     'Recluster',
     'RawDataFilterPlugin',
     'CustomActionPlugin',
-    'SplitShortISI',
     # 'AutoQuality',
     'colorSelectorPlugin',
     'customizeSelectorStatsPlugin',
